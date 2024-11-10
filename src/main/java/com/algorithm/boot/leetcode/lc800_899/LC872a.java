0ebdf4452132ecea72770847cea0f6add7db1dbf
teacs1 <845278422@qq.com>
2024-11-10 14:13:02 +0800

    LC872. 叶子相似的树-递归-迭代

---
+++ src/main/java/com/algorithm/boot/leetcode/lc800_899/LC872a.java
@@ -0,0 +1,41 @@
+package com.algorithm.boot.leetcode.lc800_899;
+
+import com.algorithm.boot.datastructure.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 872. 叶子相似的树
+ * 简单
+ * 请考虑一棵二叉树上所有的叶子，这些叶子的值按从左到右的顺序排列形成一个 叶值序列 。
+ */
+public class LC872a {
+    public static void main(String[] args) {
+        TreeNode root1 = TreeNode.arrayToTreeNode(new Integer[]{1, 2, 128});
+        TreeNode root2 = TreeNode.arrayToTreeNode(new Integer[]{1, 2, 128});
+        LC872a lc = new LC872a();
+        System.out.println(lc.leafSimilar(root1, root2));
+    }
+
+    /**
+     * 递归
+     * @param root1
+     * @param root2
+     * @return
+     */
+    public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+        List<Integer> list1 = new ArrayList<>();
+        List<Integer> list2 = new ArrayList<>();
+        getList(root1, list1);
+        getList(root2, list2);
+        if (list1.size() != list2.size()) return false;
+        return list1.equals(list2);
+    }
+
+    private void getList(TreeNode node, List<Integer> list) {
+        if (node.left == null && node.right == null) list.add(node.val);
+        if (node.left != null) getList(node.left, list);
+        if (node.right != null) getList(node.right, list);
+    }
+}
---
+++ src/main/java/com/algorithm/boot/leetcode/lc800_899/LC872b.java
@@ -0,0 +1,51 @@
+package com.algorithm.boot.leetcode.lc800_899;
+
+import com.algorithm.boot.datastructure.TreeNode;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
+/**
+ * 872. 叶子相似的树
+ * 简单
+ * 请考虑一棵二叉树上所有的叶子，这些叶子的值按从左到右的顺序排列形成一个 叶值序列 。
+ */
+public class LC872b {
+    public static void main(String[] args) {
+        TreeNode root1 = TreeNode.arrayToTreeNode(new Integer[]{1, 2});
+        TreeNode root2 = TreeNode.arrayToTreeNode(new Integer[]{1, null,2});
+        LC872b lc = new LC872b();
+        System.out.println(lc.leafSimilar(root1, root2));
+    }
+
+    /**
+     * 迭代
+     * @param root1
+     * @param root2
+     * @return
+     */
+    public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+        List<Integer> list1 = new ArrayList<>();
+        List<Integer> list2 = new ArrayList<>();
+        getList(root1, list1);
+        getList(root2, list2);
+        if (list1.size() != list2.size()) return false;
+        return list1.equals(list2);
+    }
+
+    private void getList(TreeNode node, List<Integer> list) {
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        while (node != null || !stack.isEmpty()) {
+            if (node != null) {
+                stack.push(node);
+                node = node.left;
+            } else if (node == null && !stack.isEmpty()) {
+                node = stack.pop();
+                if (node.left == null && node.right == null) list.add(node.val);
+                node = node.right;
+            }
+        }
+    }
+}
