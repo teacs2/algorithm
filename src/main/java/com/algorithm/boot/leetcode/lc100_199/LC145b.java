e7a47922cf058f0c5cfa77a1ed2646ca13f706db
teacs1 <845278422@qq.com>
2024-11-10 22:44:39 +0800

    30

--- src/main/java/com/algorithm/boot/leetcode/lc100_199/LC145b.java
+++ src/main/java/com/algorithm/boot/leetcode/lc100_199/LC145b.java
@@ -10,6 +10,11 @@ import java.util.*;
  * 简单
  */
 public class LC145b {
+    public static void main(String[] args) {
+        TreeNode root = TreeNode.arrayToTreeNode(new Integer[]{1, 2, 3, 4, 5, 6, 7});
+        LC145b lc = new LC145b();
+        lc.postorderTraversalB(root);
+    }
 
     /**
      * 递归a 栈 + 预指针
@@ -30,7 +35,7 @@ public class LC145b {
             if (root.right == null || root.right == prevAdd) {  // 如果又子树为空,或者上一个添加的节点是有节点,则添加当前节点
                 list.add(root.val);
                 prevAdd = root;
-                root = null;
+                root = null;        // 不加上会一直循环父节点和右子节点
             } else {
                 stack.push(root);
                 root = root.right;
@@ -48,7 +53,7 @@ public class LC145b {
     public List<Integer> postorderTraversalB(TreeNode root) {
         List<Integer> list = new ArrayList<>();
         Deque<TreeNode> stack = new ArrayDeque<>();
-        Set<TreeNode> visited = new TreeSet<>();
+        Set<TreeNode> visited = new HashSet<>();
         while (root != null || !stack.isEmpty()) {
             if (root == null && visited.contains(stack.peek())) {
                 list.add(stack.pop().val);
@@ -62,4 +67,48 @@ public class LC145b {
         }
         return list;
     }
+
+    /*
+    2024-11-10
+     */
+    public List<Integer> postorderTraversalA1(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        TreeNode prevAdd = null;    // 上一个加入答案的节点
+        while (root != null || !stack.isEmpty()) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            if (root.right == null || prevAdd == root.right) {
+                list.add(root.val);
+                prevAdd = root;
+            } else {
+                stack.push(root);
+                root = root.right;
+            }
+        }
+        return list;
+    }
+
+    public List<Integer> postorderTraversalB1(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        Set<TreeNode> visited = new HashSet<>();
+        while (root != null || !stack.isEmpty()) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            if (visited.contains(stack.peek())) {
+                list.add(stack.pop().val);
+            } else {
+                visited.add(stack.peek());
+                root = stack.peek().right;
+            }
+        }
+        return list;
+    }
+
 }
