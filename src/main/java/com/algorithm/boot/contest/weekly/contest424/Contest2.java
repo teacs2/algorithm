fa91df86ea06120d3dbb48daa4af6bcc85ae81c0
teacs1 <845278422@qq.com>
2024-11-11 11:22:45 +0800

    周赛初始化

---
+++ src/main/java/com/algorithm/boot/contest/weekly/contest424/Contest2.java
@@ -0,0 +1,8 @@
+package com.algorithm.boot.contest.weekly.contest424;
+
+/**
+ *
+ */
+public class Contest2 {
+
+}
---
+++ src/main/java/com/algorithm/boot/contest/weekly/contest425/Contest1.java
@@ -0,0 +1,8 @@
+package com.algorithm.boot.contest.weekly.contest425;
+
+/**
+ *
+ */
+public class Contest1 {
+
+}
