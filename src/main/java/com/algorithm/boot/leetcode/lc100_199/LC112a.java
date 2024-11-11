f36d2e4ef2da772a09f61dd56aefb9d416ba3c54
“Tea” <tiaogeng@foxmail.com>
2024-11-11 20:21:25 +0800

    纠错

--- src/main/java/com/algorithm/boot/leetcode/lc100_199/LC112a.java
+++ src/main/java/com/algorithm/boot/leetcode/lc100_199/LC112a.java
@@ -15,7 +15,6 @@ import com.algorithm.boot.datastructure.TreeNode;
 public class LC112a {
     public boolean hasPathSum(TreeNode root, int targetSum) {
         if (root == null) return false;
-        if (root.val > targetSum) return false;
         if (root.val == targetSum && root.right == null && root.left == null) return true;
         boolean left = hasPathSum(root.left, targetSum - root.val);
         boolean right = hasPathSum(root.right, targetSum - root.val);
