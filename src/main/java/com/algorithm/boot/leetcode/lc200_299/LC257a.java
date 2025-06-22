9d4796fc7043677c7cac465d6093e188fc5b7196
“Tea” <tiaogeng@foxmail.com>
2025-06-22 11:33:46 +0800

    更新文档注释

--- src/main/java/com/algorithm/boot/leetcode/lc200_299/LC257a.java
+++ src/main/java/com/algorithm/boot/leetcode/lc200_299/LC257a.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * 257. 二叉树的所有路径
+ * <a href="https://leetcode.cn/problems/binary-tree-paths/description/">257. 二叉树的所有路径</a>
  * 简单
  * 给你一个二叉树的根节点 root ，按 任意顺序 ，返回所有从根节点到叶子节点的路径。
  * 叶子节点 是指没有子节点的节点。
