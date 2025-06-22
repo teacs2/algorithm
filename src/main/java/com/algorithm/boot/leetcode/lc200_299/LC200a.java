acb9edd3433ea71fb82b8200423766d9efaaab5d
“Tea” <tiaogeng@foxmail.com>
2025-06-22 17:04:25 +0800

    200. 岛屿数量 中等

---
+++ src/main/java/com/algorithm/boot/leetcode/lc200_299/LC200a.java
@@ -0,0 +1,35 @@
+package com.algorithm.boot.leetcode.lc200_299;
+
+/**
+ * <a href="https://leetcode.cn/problems/number-of-islands/description/">200. 岛屿数量</a>
+ * 中等
+ * 给你一个由 '1'（陆地）和 '0'（水）组成的的二维网格，请你计算网格中岛屿的数量。
+ * 岛屿总是被水包围，并且每座岛屿只能由水平方向和/或竖直方向上相邻的陆地连接形成。
+ * 此外，你可以假设该网格的四条边均被水包围。
+ */
+public class LC200a {
+    public int numIslands(char[][] grid) {
+        int res = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (grid[i][j] == '1') {
+                    dfs(grid, i, j);
+                    res++;
+                }
+            }
+        }
+        return res;
+    }
+
+    private void dfs(char[][] grid, int i, int j) {
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[i].length || grid[i][j] != '1') {
+            return;
+        }
+        grid[i][j] = '2';
+        // 上下左右
+        dfs(grid, i - 1, j);
+        dfs(grid, i + 1, j);
+        dfs(grid, i, j - 1);
+        dfs(grid, i, j + 1);
+    }
+}
