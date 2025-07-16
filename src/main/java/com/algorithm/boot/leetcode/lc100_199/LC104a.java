e89985d881f1d4ac5c50f379fb8e20ea7a255e96
“Tea” <tiaogeng@foxmail.com>
2025-07-16 22:40:07 +0800

    104

--- src/main/java/com/algorithm/boot/leetcode/lc100_199/LC104a.java
+++ src/main/java/com/algorithm/boot/leetcode/lc100_199/LC104a.java
@@ -8,7 +8,9 @@ import com.algorithm.boot.datastructure.TreeNode;
  */
 public class LC104a {
     public int maxDepth(TreeNode root) {
-        if (root == null) return 0;
+        if (root == null) {
+            return 0;
+        }
         int depth1 = maxDepth(root.left);
         int depth2 = maxDepth(root.right);
         return Math.max(depth1, depth2) + 1;
@@ -18,7 +20,9 @@ public class LC104a {
     1. 2024年11月13日
      */
     public int maxDepth1(TreeNode root) {
-        if (root == null) return 0;
+        if (root == null) {
+            return 0;
+        }
         int left = maxDepth1(root.left);
         int right = maxDepth1(root.right);
         return Math.max(left, right) + 1;
@@ -28,7 +32,9 @@ public class LC104a {
     2. 2025年2月15日
      */
     public int maxDepth2(TreeNode root) {
-        if (root == null) return 0;
+        if (root == null) {
+            return 0;
+        }
         int left = maxDepth2(root.left);
         int right = maxDepth2(root.right);
         return Math.max(left, right) + 1;
