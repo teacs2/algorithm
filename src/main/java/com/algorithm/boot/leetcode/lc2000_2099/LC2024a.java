467123ae6371f50cd0e81b7edab843f7584cd202
“Tea” <tiaogeng@foxmail.com>
2025-08-07 22:29:53 +0800

    2958test

---
+++ src/main/java/com/algorithm/boot/leetcode/lc2000_2099/LC2024a.java
@@ -0,0 +1,27 @@
+package com.algorithm.boot.leetcode.lc2000_2099;
+
+public class LC2024a {
+
+    /**
+     * <a href="https://leetcode.cn/problems/maximize-the-confusion-of-an-exam/">2024. 考试的最大困扰度 中等</a><pre>
+     * 一位老师正在出一场由 n 道判断题构成的考试，每道题的答案为 true （用 'T' 表示）或者 false （用 'F' 表示）。老师想增加学生对自己做出答案的不确定性，方法是 最大化 有 连续相同 结果的题数。（也就是连续出现 true 或者连续出现 false）。
+     *
+     * 给你一个字符串 answerKey ，其中 answerKey[i] 是第 i 个问题的正确结果。除此以外，还给你一个整数 k ，表示你能进行以下操作的最多次数：
+     *
+     * 每次操作中，将问题的正确答案改为 'T' 或者 'F' （也就是将 answerKey[i] 改为 'T' 或者 'F' ）。
+     * 请你返回在不超过 k 次操作的情况下，最大 连续 'T' 或者 'F' 的数目。
+     *
+     * n == answerKey.length
+     * 1 <= n <= 5 * 10^4
+     * answerKey[i] 要么是 'T' ，要么是 'F'
+     * 1 <= k <= n
+     * </pre>
+     * @param answerKey
+     * @param k
+     * @return
+     */
+    public int maxConsecutiveAnswers(String answerKey, int k) {
+
+        return 0;
+    }
+}
